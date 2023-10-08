2a7b19c4d886a40589c3526110aa32c41133f169
Julien Levieux <julien.levieux@drotek.com>
2023-10-08 09:55:23 +0200

    move code in src file + add tsconfig.json

---
+++ src/dropdown.tsx
@@ -0,0 +1,44 @@
+
+import React from "react";
+import { useState } from "react";
+
+export interface DropdownProps {
+  title: string;
+  children: any[];
+}
+
+import "./dropdown.css";
+
+export const Dropdown = (props: DropdownProps) => {
+  const [open, setOpen] = useState(false);
+
+  const [current_option, setCurrentOption] = useState(props.children[0]);
+
+  const toggle = () => setOpen(!open);
+
+  return (
+    <div className="dropdown">
+      <div className="dropdown__toggle" onClick={toggle}>
+        {current_option}
+      </div>
+      {open && (
+        <div>
+            {props.children.map((option, index) => (
+              <div
+                key={index}
+                className="dropdown__menu__item"
+                onClick={() => {
+                  setCurrentOption(option);
+                  setOpen(false);
+                }}
+              >
+                {option}
+              </div>
+            ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+
